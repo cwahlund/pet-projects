636fb743773ad85f82c6ef13f0244e07a3fabc53
cwahlund <wahlund@icloud.com>
2023-08-01 14:05:01 -0700

    Added pet parks data handler code

--- pet-parks/src/main/java/pet/park/dao/ContributorDao.java
+++ pet-parks/src/main/java/pet/park/dao/ContributorDao.java
@@ -3,7 +3,10 @@ package pet.park.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pet.park.entity.Contributor;
+import java.util.Optional;
 
 public interface ContributorDao extends JpaRepository<Contributor, Long> {
+
+	Optional<Contributor> findByContributorEmail(String contributorEmail);
 
 }
